36a90ce36a38c87ac9b0a271c56b6bbbce358280
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-08-26 17:25:13 -0300

    chore: fix #8 and starting #17

---
+++ dist/precache-manifest.70382ff52960379912a850a4783c97a3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b51c1c487c5690edafcd",
+    "url": "/css/app.4f5dc6e6.css"
+  },
+  {
+    "revision": "b51c1c487c5690edafcd",
+    "url": "/js/app.b7465c5c.js"
+  },
+  {
+    "revision": "f8e503b1697f15071f60",
+    "url": "/js/chunk-vendors.1835b0b2.js"
+  },
+  {
+    "revision": "666c2a1aab5023b061d91e5ad78dfb29",
+    "url": "/index.html"
+  }
+];
