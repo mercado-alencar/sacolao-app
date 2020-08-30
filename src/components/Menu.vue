20f4cc786c955d4b6b789766f3dd07e7a1e50897
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-08-30 09:40:07 -0300

    chore: netlify configuratin

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,11 +1,8 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="#">Mercado Alencar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+
+  <ul class="navbar-nav">
       <li class="nav-item">  
           <router-link class="nav-link" to="/venda">Venda </router-link>      
       </li>
@@ -16,7 +13,6 @@
          <router-link class="nav-link" to="/rotas">Rotas </router-link>
       </li> -->
     </ul>
-  </div>
 </nav>
 </template>
 
