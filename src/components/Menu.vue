7f9f4c8fec3dce0d9a2f680411e1bc28fc1334dd
Alan Balen Schio <alanbschio@gmail.com>
2022-06-19 20:44:56 -0400

    fixing problems and http

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -6,9 +6,9 @@
       <li class="nav-item">  
           <router-link class="nav-link" to="/venda">Venda </router-link>      
       </li>
-       <li class="nav-item">  
+       <!-- <li class="nav-item">  
           <router-link class="nav-link" to="/sorteio">Sorteio </router-link>      
-      </li>
+      </li> -->
        <li class="nav-item">
         <router-link class="nav-link" to="/entregas">Entregas </router-link>
         </li>
